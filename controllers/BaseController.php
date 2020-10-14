df22d4e7937aaf4997633a306f25d9d26105074a
Toria <toria.andria@gmail.com>
2020-10-14 17:10:11 +0400

    Create base Controller which will be extended for all other controllers

---
+++ controllers/BaseController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace frontend\modules\api\v1\controllers;
+
+use yii\rest\ActiveController;
+
+
+/**
+ * Class BasicController
+ */
+class BaseController extends ActiveController
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // remove corsFilter filter
+        unset($behaviors['corsFilter']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [
+                    'X-Pagination-Per-Page',
+                    'X-Pagination-Total-Count',
+                    'X-Pagination-Current-Page',
+                    'X-Pagination-Page-Count',
+                ],
+            ]
+        ];
+
+        return $behaviors;
+    }
+}
