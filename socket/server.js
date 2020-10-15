9baea38946f13b1eabdee67e3c250d1459cc52cc
Toria <toria.andria@gmail.com>
2020-10-16 00:06:32 +0400

    Remove unused lines

--- socket/server.js
+++ socket/server.js
@@ -6,18 +6,6 @@ const cors = require('cors');
 const config = require('./config')
 
 
-const whitelist = [] //white list for cron.
-
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-};
-
 app.use(cors(config.whiteList));
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({extended: true}));
